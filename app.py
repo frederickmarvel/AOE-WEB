8e60b8a97ff48767c3e41d68589c9c208b746571
Frederick Marvel <frederickmarvel@Fredericks-MacBook-Pro.local>
2024-11-06 13:28:53 +0700

    major fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
+
 app = Flask(__name__)
 app.secret_key = "AN!@#J#Y"
 
@@ -18,7 +19,6 @@ def services():
 def sustainability():
     return make_response(render_template("sustainability.html"))
 
-
 @app.route('/form_entry', methods=["POST", "GET"])
 def form_entry():
     if request.method == "POST":
@@ -30,9 +30,3 @@ def form_entry():
         msg = "Halo Alpha One Energi,%20saya" + name + "\n" + "Email : " + email + "\n" + "Perusahaan: " + company + "\n" + "Lokasi: " + location + "\n" + message
         msg = msg.replace(" ", "%20")
         return redirect(f"https://api.whatsapp.com/send?phone=6285171650250&text={msg}")
-
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True, threaded=False)
-
